e51dffd47a79c85718670fde41376d9c5b2e1f5b
programista10kpln <programista10kpln@proton.me>
2022-06-16 11:24:34 +0200

    setting up home page

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ load_dotenv('venv/.env')
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+@app.route('/home')
+def home():
+    url = "http://api.openweathermap.org/geo/1.0/direct"
+
+    querystring = {"q": "London", "limit": "1", "appid": os.getenv('api_key')}
+
+    geo = requests.request("GET", url, params=querystring)
+    input_city = geo.json()[0]['local_names']['pl']
+    lat = geo.json()[0]['lat']
+    lon = geo.json()[0]['lon']
+    return render_template('home.html', input_city=input_city, lat=lat, lon=lon)
+
+
+@app.route('/forecast', methods=['GET'])
 def get_weather():
     url = "https://api.openweathermap.org/data/2.5/forecast"
 
@@ -19,8 +33,8 @@ def get_weather():
 
     city = response.json()['city']
     forecast = response.json()['list']
-    return render_template("main.html", city=city, forecast=forecast)
+    return render_template('forecast.html', city=city, forecast=forecast)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
